c1c626aab92e875939bfa9072d64b720f1f497f4
isabellaim <iimartin@espol.edu.ec>
2024-08-11 14:38:53 -0500

    Inicios de cambios en gestion cliente

---
+++ proyectoSBD2P/src/main/java/ec/edu/espol/proyectosbd2p/GestionEmpleadosController.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ec.edu.espol.proyectosbd2p;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author isabella
+ */
+public class GestionEmpleadosController implements Initializable {
+
+    @FXML
+    private ImageView imgLogo;
+    @FXML
+    private Button botonInicio;
+    @FXML
+    private VBox containerFAuto;
+    @FXML
+    private VBox filtroAuto;
+    @FXML
+    private VBox plantillaAutos;
+    @FXML
+    private ImageView planImg;
+    @FXML
+    private Text plantVehName;
+    @FXML
+    private Label planVehAnio;
+    @FXML
+    private Text planVehKm;
+    @FXML
+    private Text planVehCiu;
+    @FXML
+    private Text planVehNego;
+    @FXML
+    private Text planVehPrecio;
+    @FXML
+    private HBox pestanias;
+    @FXML
+    private HBox pagesInd;
+    @FXML
+    private Label lblNumPagina;
+    @FXML
+    private Label lblNumPagina1;
+    @FXML
+    private Label lblNumPagina2;
+    @FXML
+    private TextField tbRuc;
+    @FXML
+    private TextField tbNombreEmpresa;
+    @FXML
+    private TextField tbDireccion;
+    @FXML
+    private TextField tbSitioWeb;
+    @FXML
+    private Button btnBuscar;
+    @FXML
+    private GridPane gridClientes;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void irInicio(ActionEvent event) {
+    }
+
+
+    @FXML
+    private void buscarFiltros(ActionEvent event) {
+    }
+
+
+    @FXML
+    private void prevPag(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextPag(ActionEvent event) {
+    }
+    
+}

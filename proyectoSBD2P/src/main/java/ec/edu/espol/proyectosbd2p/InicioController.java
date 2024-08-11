f0c52719107597e4796ea4d6c80740aed1330165
Annabella Sánchez <ansaguzm@espol.edu.ec>
2024-08-11 15:32:23 -0500

    Imagenes en pantalla Inicio

--- proyectoSBD2P/src/main/java/ec/edu/espol/proyectosbd2p/InicioController.java
+++ proyectoSBD2P/src/main/java/ec/edu/espol/proyectosbd2p/InicioController.java
@@ -6,7 +6,11 @@ package ec.edu.espol.proyectosbd2p;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -15,6 +19,29 @@ import javafx.fxml.Initializable;
  */
 public class InicioController implements Initializable {
 
+    @FXML
+    private ImageView imgLogo;
+    @FXML
+    private Button consultarSegmento;
+    @FXML
+    private Button consultarProductoTienda;
+    @FXML
+    private Button consultarAnuncioWeb;
+    @FXML
+    private Button consultarEmpleado;
+    @FXML
+    private Button consultarCliente;
+    @FXML
+    private ImageView imgSegmento;
+    @FXML
+    private ImageView imgProductoTienda;
+    @FXML
+    private ImageView imgAnuncioWeb;
+    @FXML
+    private ImageView imgEmpleado;
+    @FXML
+    private ImageView imgCliente;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +49,13 @@ public class InicioController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void clickEnBuscarSegmento(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnBuscarVehiculos(ActionEvent event) {
+    }
     
 }

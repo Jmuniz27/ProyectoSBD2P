ab9d33a5fb17e8618aa43df089951e307d9cddd6
LockHurb <zdiaz@espol.edu.ec>
2024-08-12 01:34:02 -0500

    Añadir Clientes y empleados :0

---
+++ proyectoSBD2P/src/main/java/ec/edu/espol/proyectosbd2p/AnadirClienteController.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ec.edu.espol.proyectosbd2p;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author zahid
+ */
+public class AnadirClienteController implements Initializable {
+
+    @FXML
+    private Button botonInicio;
+    @FXML
+    private TextField txtRucCliente;
+    @FXML
+    private TextField txtNombreCliente;
+    @FXML
+    private TextField txtDescripcionCliente;
+    @FXML
+    private TextField txtDireccionEmpresa;
+    @FXML
+    private TextField txtSitioWebEmpresa;
+    @FXML
+    private TextField txtPersonaContactoEmpresa;
+    @FXML
+    private Button btnAnadirCliente;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void irInicio(ActionEvent event) {
+    }
+
+    @FXML
+    private void anadirCliente(ActionEvent event) {
+    }
+    
+}
